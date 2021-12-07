6a2a9924c0191eafbb4ee820f5fc5d26159fa739
sandeepkumar1234 <54533323+sandeepkumar1234@users.noreply.github.com>
2021-12-07 20:52:43 +0530

    Update ERRequestCreationTest.java

--- src/test/java/com/iftas/pageTest/ERRequestCreationTest.java
+++ src/test/java/com/iftas/pageTest/ERRequestCreationTest.java
@@ -83,21 +83,21 @@ public class ERRequestCreationTest extends LaunchandLoginTest {
 	  
 	  Thread.sleep(4000);
 	  
-	  // s.click(System.getProperty("user.dir")+"//Images//Browsepfx.png"); //
-	  //s.click(System.getProperty("user.dir")+"//Images//pfxfile.png"); //
-	  //s.click(System.getProperty("user.dir")+"//Images//pfxfileopen.png"); //
+	  // s.click(System.getProperty("user.dir")+"/Images/Browsepfx.png"); //
+	  //s.click(System.getProperty("user.dir")+"/Images/pfxfile.png"); //
+	  //s.click(System.getProperty("user.dir")+"/Images/pfxfileopen.png"); //
 	  
-	  s.click(System.getProperty("user.dir")+"//Images//pfxpasswordfield.png");
+	  s.click(System.getProperty("user.dir")+"/Images/pfxpasswordfield.png");
 	  
-	  s.click(System.getProperty("user.dir") + "//Images//SelectFileImage.png");
+	  s.click(System.getProperty("user.dir") + "/Images/SelectFileImage.png");
 	  
-	  s.type(System.getProperty("user.dir")+"//PFXFile//RBIH0000000.pfx");
+	  s.type(System.getProperty("user.dir")+"/PFXFile/RBIH0000000.pfx");
 	  
-	  s.click(System.getProperty("user.dir") + "//Images//pfxpasswordfield.png");
+	  s.click(System.getProperty("user.dir") + "/Images/pfxpasswordfield.png");
 	  
 	  s.type("pfxfile123");
 	  
-	  s.click(System.getProperty("user.dir") + "//Images//Okbutton_pfxwindow.png");
+	  s.click(System.getProperty("user.dir") + "/Images/Okbutton_pfxwindow.png");
 	  
 	  Thread.sleep(10000);
 	  
@@ -203,9 +203,9 @@ public class ERRequestCreationTest extends LaunchandLoginTest {
 	  
 	  Thread.sleep(10000);
 	  
-	  s.click(System.getProperty("user.dir") + "//Images//security check box.png");
+	  s.click(System.getProperty("user.dir") + "/Images/security check box.png");
 	  
-	  s.click(System.getProperty("user.dir") + "//Images//Runbutton.png");
+	  s.click(System.getProperty("user.dir") + "/Images/Runbutton.png");
 	  
 	  driver.findElement(By.xpath(
 	  "//table[@id='enhcdRcnReqListingTableId']/tbody/tr[1]")).click();
@@ -239,15 +239,15 @@ public class ERRequestCreationTest extends LaunchandLoginTest {
 
 		Thread.sleep(4000);
 
-		s.click(System.getProperty("user.dir")+"//Images//SelectFileImage.png");
+		s.click(System.getProperty("user.dir")+"/Images/SelectFileImage.png");
 
-		s.type(System.getProperty("user.dir") + "//PFXFile//RBIH0000000.pfx");
+		s.type(System.getProperty("user.dir") + "/PFXFile/RBIH0000000.pfx");
 
-		s.click(System.getProperty("user.dir") + "//Images//pfxpasswordfield.png");
+		s.click(System.getProperty("user.dir") + "/Images/pfxpasswordfield.png");
 
 		s.type("pfxfile123");
 
-		s.click(System.getProperty("user.dir") + "//Images//Okbutton_pfxwindow.png");
+		s.click(System.getProperty("user.dir") + "/Images/Okbutton_pfxwindow.png");
 
 		Thread.sleep(10000);
 
@@ -273,9 +273,9 @@ public class ERRequestCreationTest extends LaunchandLoginTest {
 		
 		Thread.sleep(20000);
 
-		s.click(System.getProperty("user.dir") + "//Images//security check box.png");
+		s.click(System.getProperty("user.dir") + "/Images/security check box.png");
 
-		s.click(System.getProperty("user.dir") + "//Images//Runbutton.png");
+		s.click(System.getProperty("user.dir") + "/Images/Runbutton.png");
 
 		//driver.findElement(By.xpath("//table[@id='enhcdRcnReqListingTableId']/tbody/tr[1]")).click();
 
